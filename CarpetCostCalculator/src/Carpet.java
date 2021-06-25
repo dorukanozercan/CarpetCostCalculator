0b28e05e70ba16e5e87af2474040d6207df59826
dorukanozercan <42218198+dorukanozercan@users.noreply.github.com>
2021-06-25 18:16:08 +0300

    added files

---
+++ CarpetCostCalculator/src/Carpet.java
@@ -0,0 +1,17 @@
+public class Carpet {
+
+    private double cost;
+
+    public Carpet (double cost) {
+        if (cost < 0) {
+            cost = 0;
+        } else {
+            this.cost = cost;
+        }
+
+    }
+
+    public double getCost () {
+        return cost;
+    }
+}
